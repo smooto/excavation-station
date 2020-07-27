f563f2f3a8225b3b10e739d5b0e0c06edf69d9b1
smooto <melissasmoot@live.com>
2020-07-27 13:19:57 -0700

    Clean up deprecated code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import { grid, buryTreasure, addFirstLayer } from './engine/grid';
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -31,9 +29,6 @@ function App() {
   );
 }
 
-// grid component
-// variables: treasure coords, tile coords
-
 function toggleColor() {
   console.log('toggled')
 }
@@ -42,30 +37,5 @@ function toggleColor() {
 function makeGrid(rows, columns) {
   return Array(rows * columns).fill(undefined)
 }
-
-// function fossilSpot(rows, columns) {
-//   // return {
-//   //   row: Math.floor(Math.random() * rows),
-//   //   col: Math.floor(Math.random() * columns)
-//   // }
-//   setFossil(Math.floor(Math.random() * (rows * columns)))
-// }
-
-// function buryTreasure(grid) {
-  // const randomRow = Math.floor(Math.random() * grid.length);
-  // const randomCol = Math.floor(Math.random() * grid[0].length);
-
-//   return grid.map((row, rowIndex) => {
-//     if (rowIndex === randomRow) {
-//       return row.map((column, colIndex) => {
-//         if (colIndex === randomCol) {
-//           return <div class="treasure"></div>
-//         }  
-//         return column
-//       })
-//     }
-//     return row
-//   })
-// }
 
 export default App;
