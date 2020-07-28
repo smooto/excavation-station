4fa55fc0b955e131ce957d6ed473da197a122909
smooto <melissasmoot@live.com>
2020-07-27 21:44:26 -0700

    Add completion state to tiles

--- src/Cell.jsx
+++ src/Cell.jsx
@@ -8,16 +8,19 @@ const gameLevels = {
 export default function Cell({ passClick, level, hasFossil }) {
   const [tile, setTile] = useState(getTile(level))
   const [clickCount, setClickCount] = useState(0)
-
+  const [complete, setComplete] = useState(false)
+  
   useEffect(() => {
     passClick()
+    clickCount === tile ? setComplete(true) : setComplete(false);
   }, [clickCount])
 
   return (
     <div class="cell" onClick={() => setClickCount(clickCount + 1)}>
       <p>
-        {tile}
+        {tile + ' ' + clickCount}
         {hasFossil && <span>X</span>}
+        {complete && <span>done</span>}
       </p>
     </div>
   )
